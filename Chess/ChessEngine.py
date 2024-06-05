47d838d4c05d07b56e10cfc595ab26e45baa1a40
Connor Pierce <connor_pierce@ymail.com>
2024-06-04 22:35:45 -0300

    Almost board done

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,22 @@
+"""
+This class is responsibe for storing all information about the game, Also responsible for determining all valid moves
+"""
+class GameState():
+    def __init__(self):
+        #board is 8x8 2D list, where each element has 2 characters
+        #first character is colour
+        #second is type of piece
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
